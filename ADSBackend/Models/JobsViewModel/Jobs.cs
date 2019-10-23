73f402a042d297c47e603ee2cd2f5fdb8bfb92a2
Nicholas Losardo <nick1108@me.com>
2019-10-23 11:27:35 -0400

    rip job

--- ADSBackend/Models/JobsViewModel/Jobs.cs
+++ ADSBackend/Models/JobsViewModel/Jobs.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace ADSBackend.Models.JobViewModel
+namespace ADSBackend.Models.JobsViewModel
 {
     public class Jobs
     {
-
+        public string AJobs { get; set; }
+        public string JobTitle { get; set; }
+        [Display(Name = "Job")]
+        public string JobDetail { get; set; }
+        [Display(Name = "Job Description")]
+        public string Wage { get; set; }
+        [Display(Name = "Wage")]
+        public string Hours { get; set; }
+        [Display(Name = "Hours")]
+        public string Test { get; set; }
     }
+
 }
