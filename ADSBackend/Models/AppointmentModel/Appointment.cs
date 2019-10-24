e4267747e9f831bfc43deba51bf3e1753674a900
Nicholas Losardo <nick1108@me.com>
2019-10-24 12:49:01 -0400

    Appointment Page Made and almost working

---
+++ ADSBackend/Models/AppointmentModel/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADSBackend.Models.AppointmentModel
+{
+    public class Jobs
+    {
+        [Key]
+        public string Appointment { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Councelor")]
+        public string Councelor { get; set; }
+        [DataType(DataType.Currency)]
+        [Display(Name = "Hourly Wage")]
+        public string Wage { get; set; }
+        [DataType(DataType.Duration)]
+        [Display(Name = "Work Hours")]
+        public string Hours { get; set; }
+        public string Test { get; set; }
+    }
+}
