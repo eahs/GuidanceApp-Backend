2de915c741007e4b346887677a69f133c9551362
Nicholas Losardo <nick1108@me.com>
2019-11-22 12:40:19 -0500

    appointment broke 2 electric boogaloo

--- ADSBackend/Controllers/HomeController.cs
+++ ADSBackend/Controllers/HomeController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ADSBackend.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ADSBackend.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var appointments = await _context.Appointment.ToListAsync();
             return View();
         }
     }
