dab1a2b0af4923283a60cbc8ce4c8d4bc28ae1fe
Nicholas Losardo <nick1108@me.com>
2020-02-11 11:30:39 -0500

    Made table on dashboard and some other stuff
    epic

--- ADSBackend/Controllers/AdminController.cs
+++ ADSBackend/Controllers/AdminController.cs
@@ -1,15 +1,20 @@
 ﻿using ADSBackend.Models.HomeViewModels;
+using ADSBackend.Data;
 using ADSBackend.Models.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace ADSBackend.Controllers
 {
     [Authorize]
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
 
         public AdminController(UserManager<ApplicationUser> userManager)
@@ -23,7 +28,8 @@ namespace ADSBackend.Controllers
             {
                 User = await _userManager.GetUserAsync(User)
             };
-
+            var student = await _context.Staff.ToListAsync();
+            
             return View(viewModel);
         }
     }
